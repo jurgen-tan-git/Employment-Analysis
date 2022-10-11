3e4636e3175daa545f2791ff5dada9b76d488b4a
jurgen-tan-git <56992789+jurgen-tan-git@users.noreply.github.com>
2022-10-11 12:24:28 +0800

    Added setup file for theme

--- streamlit.py
+++ streamlit.py
@@ -1,21 +1,21 @@
-import matplotlib
 import pandas as pd
-import streamlit as st
-
-st.set_page_config(layout="wide")
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 
+import matplotlib
 matplotlib.use('Agg')
+
+import streamlit as st
+st.set_page_config(layout="wide")
 
 # Title and Sidebar information
 st.title('Employment Change Dataset')
 st.sidebar.subheader(' Filter ')
 data = st.radio("Which Dataset To View",
-                 (
-                     'Employment Change', 'Employment By Gender and Education',
-                     'Labour Force Status By Age Group'))
+                (
+                    'Employment Change', 'Employment By Gender and Education',
+                    'Labour Force Status By Age Group'))
 
 if data == "Employment Change":
     file = "mrsd_39_annual_ emp_chng_by_ind_and_res_stat_29072022"
@@ -34,21 +34,18 @@ columns = original.columns.values
 #     columns={"year": "Year", "industry2": "Industry", "employment_change": "Change"})
 
 
-
-
 explore = st.sidebar.radio("Data Filters", ("Employment Data", "Quick Look", "Columns"))
-
 
 if explore == "Employment Data":
     st.subheader("Employment Data")
-# Selecting Of Columns For Display
+    # Selecting Of Columns For Display
     columns_select = st.multiselect("Select The Columns To View",
-                                    columns,default=columns)
-# Year slidebar to choose year range
+                                    columns, default=columns)
+    # Year slidebar to choose year range
     start = int(min(original.year))
     end = int(max(original.year))
     start_year, end_year = st.select_slider('Select the years to view',
-                                            options=list(range(start, end+1)),value=(start, end))
+                                            options=list(range(start, end + 1)), value=(start, end))
     st.write('You selected the year range of', start_year, 'and', end_year)
 
     original = original[(original.year >= start_year) & (original.year <= end_year)]
@@ -78,36 +75,35 @@ elif explore == "Columns":
     st.subheader('Show Columns List')
     st.write(original.columns.to_list())
 
-
 # if st.sidebar.checkbox('Graph'):
 if data == "Employment Change":
     st.subheader(data + " Sunburst From 2017 To 2021")
     df = df[df.year >= 2017]
     df["employment_change"] = df["employment_change"].astype(float)
-    fig = px.sunburst(df, path=['year', "res_stat",'industry1',"employment_change"],
-                      color="employment_change",color_continuous_scale='RdBu')
+    fig = px.sunburst(df, path=['year', "res_stat", 'industry1', "employment_change"],
+                      color="employment_change", color_continuous_scale='RdBu')
     st.plotly_chart(fig)
 
 elif data == "Employment By Gender and Education":
     st.subheader(data + ' Barplot From 2017 To 2021')
-# st.info("Please set a year range less than 5 years")
-# columnX = st.sidebar.selectbox("X (Choose a column). Try Selecting Year:", df.columns)
-# columnY = st.sidebar.selectbox("Y (Choose a column). Try Selecting Change", df.columns)
-# hueopt = st.sidebar.selectbox("Hue (Choose a column). Try Selecting Industry", df.columns)
-#     fig = plt.figure(figsize=(15, 8))
-# sns.barplot(data=df, x="Year", y="Change", hue="Industry")
-#     sns.barplot(x=columnX, y=columnY, hue=hueopt, data=df, palette="Paired")
-#     st.pyplot(fig)
+    # st.info("Please set a year range less than 5 years")
+    # columnX = st.sidebar.selectbox("X (Choose a column). Try Selecting Year:", df.columns)
+    # columnY = st.sidebar.selectbox("Y (Choose a column). Try Selecting Change", df.columns)
+    # hueopt = st.sidebar.selectbox("Hue (Choose a column). Try Selecting Industry", df.columns)
+    #     fig = plt.figure(figsize=(15, 8))
+    # sns.barplot(data=df, x="Year", y="Change", hue="Industry")
+    #     sns.barplot(x=columnX, y=columnY, hue=hueopt, data=df, palette="Paired")
+    #     st.pyplot(fig)
 
-# Employment By Age and Education
-# df_age_edu = pd.read_csv("Datasets/employed_15_sex_edu_age_yr.csv").rename(columns={"year": "Year",
-#                                                                                     "sex": "Sex",
-#                                                                                     "age": "Age",
-#                                                                                     "edu_1": "Education",
-#                                                                                     "employed": "Employed"
-#                                                                                     }).replace("-", 0)
+    # Employment By Age and Education
+    # df_age_edu = pd.read_csv("Datasets/employed_15_sex_edu_age_yr.csv").rename(columns={"year": "Year",
+    #                                                                                     "sex": "Sex",
+    #                                                                                     "age": "Age",
+    #                                                                                     "edu_1": "Education",
+    #                                                                                     "employed": "Employed"
+    #                                                                                     }).replace("-", 0)
     df.employed = pd.to_numeric(df.employed)
-    df = df[(df.year >= 2017)& (df.age != "15-19")]
+    df = df[(df.year >= 2017) & (df.age != "15-19")]
     df = df.groupby(["year", "edu_1"]).sum()
     df = df.reset_index()
 
@@ -135,7 +131,7 @@ elif data == "Labour Force Status By Age Group":
     labels = ["20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64"]
 
     # Create subplots: use 'domain' type for Pie subplot
-    fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+    fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
     fig.add_trace(go.Pie(labels=labels, values=df["employed"][df.year == 2019], name="2019"),
                   1, 1)
     fig.add_trace(go.Pie(labels=labels, values=df["employed"][df.year == 2020], name="2020"),
